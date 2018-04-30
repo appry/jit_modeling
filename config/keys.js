9fd0a7dc34e0548d7abd13582d212014f3a37433
Vladislav Rakovskiy <rvappry@yandex.ru>
2018-05-01 00:31:39 +0500

    Added jwt and models

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://vlad:vlad@ds115729.mlab.com:15729/devconnector"
+  mongoURI: "mongodb://vlad:vlad@ds115729.mlab.com:15729/devconnector",
+  secretOrKey: "secret"
 };
